2bbc24096dd0822e6c6a9c6c246c50d222ca887e
Cristian <cristianvillalobos666@gmail.com>
2024-11-24 16:52:56 -0300

    test

--- db/config.ts
+++ db/config.ts
@@ -1,49 +1,60 @@
 // db/config.ts
-import { column, defineDb, defineTable } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
 
 const User = defineTable({
   columns: {
-    id: column.text({ primaryKey: true, unique: true }), 
-    username: column.text({ unique: true }),  
-    email: column.text({ unique: true }),     
-    password: column.text(),                 
-    createdAt: column.date({ default: new Date() }), 
-    role: column.text({ references: () => Role.columns.id }), 
+    id: column.text({ primaryKey: true, unique: true }),
+    username: column.text({ unique: true }),
+    email: column.text({ unique: true }),
+    password: column.text(),
+    createdAt: column.date({ default: new Date() }),
+    role: column.text({ references: () => Role.columns.id }),
   },
 });
- 
+
 const Role = defineTable({
   columns: {
-    id: column.text({ primaryKey: true }),  
-    name: column.text(), 
+    id: column.text({ primaryKey: true }),
+    name: column.text(),
   },
 });
 
 const Post = defineTable({
   columns: {
-    id: column.text({ primaryKey: true, unique: true }), 
-    userId: column.text({ references: () => User.columns.id }),  
-    content: column.text(), 
-    imageUrl: column.text(),  
-    createdAt: column.date({ default: new Date() }), 
+    id: column.text({ primaryKey: true, unique: true }),
+    userId: column.text({ references: () => User.columns.id }),
+    content: column.text(),
+    imageUrl: column.text(),
+    createdAt: column.date({ default: new Date() }),
   },
 });
 
 const PostImage = defineTable({
   columns: {
-    id: column.text({ primaryKey: true }),  
-    postId: column.text({ references: () => Post.columns.id }),  
-    imageUrl: column.text(),  
+    id: column.text({ primaryKey: true }),
+    postId: column.text({ references: () => Post.columns.id }),
+    imageUrl: column.text(),
   },
 });
 
 const Comment = defineTable({
   columns: {
-    id: column.text({ primaryKey: true }),  
-    postId: column.text({ references: () => Post.columns.id }),  
-    userId: column.text({ references: () => User.columns.id }),  
-    content: column.text(),  
-    createdAt: column.date({ default: new Date() }), 
+    id: column.text({ primaryKey: true }),
+    postId: column.text({ references: () => Post.columns.id }),
+    userId: column.text({ references: () => User.columns.id }),
+    content: column.text(),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
+const Author = defineTable({
+  columns: { id: column.number({ primaryKey: true }), name: column.text() },
+});
+
+const Comments = defineTable({
+  columns: {
+    authorId: column.number({ references: () => Author.columns.id }),
+    body: column.text(),
   },
 });
 
@@ -54,5 +65,7 @@ export default defineDb({
     Post,
     PostImage,
     Comment,
+    Comments,
+    Author,
   },
 });
